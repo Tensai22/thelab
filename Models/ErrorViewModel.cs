494f1eeded45c97b9844baac2aeb402b2b36debc
Tensai <bakooofficial@gmail.com>
2025-03-12 15:31:04 +0500

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
