e46548ebcf1cb27b1cd9b54efb0386b2a936359a
Tensai <bakooofficial@gmail.com>
2025-03-12 18:29:06 +0500

    Добавил в layout переход на другие страницы с помощью html helper

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheLab.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Testimonials() 
+        {
+            return View();
+        }
+
+    }
+}
