5a630b6259936b0479e93919af11d953185b9779
Bakay <bakayzhazira@gmail.com>
2025-03-13 08:32:53 +0500

    Layout, регистрация

---
+++ Controllers/WorksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheLab.Controllers
+{
+    public class WorksController : Controller
+    {
+        public IActionResult Works1()
+        {
+            return View();
+        }
+
+        public IActionResult Works2()
+        {
+            return View();
+        }
+
+    }
+}
